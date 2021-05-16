5d8c2ffb00597674f1301d924780d432f5535c17
MartinGC94 <MGC94@outlook.dk>
2021-05-16 15:07:44 +0200

    Update themes and VS code settings

--- Themes/VS code themes/Custom Powershell Theme/themes/CreateThemes.ps1
+++ Themes/VS code themes/Custom Powershell Theme/themes/CreateThemes.ps1
@@ -1,11 +1,11 @@
 $ThemeData=Get-Content -Path "$PSScriptRoot\ThemeData.json" -Raw | ConvertFrom-Json
 [pscustomobject]@{
-    name="ISE Dark Theme"
+    name="Powershell Dark Theme"
     colors=$ThemeData.DarkColors
     tokenColors=$ThemeData.tokenColors | Select-Object -ExcludeProperty DarkSettings,LightSettings -Property *,@{Name="settings";Expression={$_.DarkSettings}}
-} | ConvertTo-Json -Depth 5 -Compress | Set-Content -Path "$PSScriptRoot\ISE-Dark-Theme.json"
+} | ConvertTo-Json -Depth 5 -Compress | Set-Content -Path "$PSScriptRoot\Powershell-Dark-Theme.json"
 [pscustomobject]@{
-    name="ISE Light Theme"
+    name="Powershell Light Theme"
     colors=$ThemeData.LightColors
     tokenColors=$ThemeData.tokenColors | Select-Object -ExcludeProperty DarkSettings,LightSettings -Property *,@{Name="settings";Expression={$_.LightSettings}}
-} | ConvertTo-Json -Depth 5 -Compress | Set-Content -Path "$PSScriptRoot\ISE-Light-Theme.json"
+} | ConvertTo-Json -Depth 5 -Compress | Set-Content -Path "$PSScriptRoot\Powershell-Light-Theme.json"
